cd3fb4cae8fa7d519aa0d5a32f48d374ada6f3a1
trung0412 <letrung041201@gmail.com>
2022-06-01 01:25:09 +0700

    first commit

---
+++ mvcGGMobile/mvcGGMobile/Controllers/GGMobileController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcGGMobile.Models;
+
+namespace GGMobile.Controllers
+{
+    public class GGMobileController : Controller
+    {
+        //database
+        dbGGMobileDataContext data = new dbGGMobileDataContext();
+
+        private List<SanPham> LaySanphammoi (int count)
+        {
+            return data.SanPhames.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        // GET: GGMobile
+        public ActionResult Index()
+        {
+            // lay 5 sp
+            var sanphammoi = LaySanphammoi(5);
+            return View(sanphammoi);
+        }
+        public ActionResult Hang()
+        {
+            var hang = from hangsp in data.Hangs select hangsp;
+            return PartialView(hang);
+        }
+        public ActionResult SPTheoHang(int id)
+        {
+            var SanPham = from sp in data.SanPhames where sp.MaHang==id select sp;
+            return PartialView(SanPham);
+        }
+        public ActionResult Details(int id)
+        {
+            var SanPham = from sp in data.SanPhames
+                          where sp.MaSP == id
+                          select sp;
+            return View(SanPham.Single());
+        }
+    }
+}
---
+++ mvcGGMobile/mvcGGMobile/Controllers/NguoidungController.cs
@@ -0,0 +1,107 @@
+﻿using mvcGGMobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcGGMobile.Controllers
+{
+    public class NguoidungController : Controller
+    {
+        dbGGMobileDataContext db = new dbGGMobileDataContext();
+        // GET: Nguoidung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection,KHACHHANG kh)
+        {
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var nhaplaimatkhau = collection["Nhaplaimatkhau"];
+            var diachi = collection["Diachi"];
+            var email = collection["Email"];
+            var dienthoai = collection["Dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["Ngaysinh"]);
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên không được bỏ trống";
+            }
+            else if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(nhaplaimatkhau))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email không đươc bỏ trống";
+            }
+            if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi6"] = "Phải nhập SĐT";
+            }
+            else
+            {
+                
+                //gan gia tri cho doi tuong dc tao moi
+                kh.HoTen = hoten;
+                kh.Taikhoan = tendn;
+                kh.Matkhau = matkhau;
+                kh.Email = email;
+                kh.DiachiKH = diachi;
+                kh.DienthoaiKH = dienthoai;
+                kh.Ngaysinh = DateTime.Parse(ngaysinh);
+                db.KHACHHANGs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("Dangnhap");
+            }
+            return this.Dangky();
+        }
+        [HttpGet]
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+        public ActionResult Dangnhap(FormCollection collection)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phai nhap ten dn";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phai nhap kn";
+            }
+            else
+            {
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.Taikhoan == tendn && n.Matkhau == matkhau);
+                if (kh != null)
+                {
+                    ViewBag.Thongbao = "Dang nhap thanh cong";
+                    Session["Taikhoan"] = kh;
+                }
+                else
+                    ViewBag.Thongbao = "Ten dang nhap or mk ko dung";
+            }
+            return View();
+
+        }
+    }
+}
